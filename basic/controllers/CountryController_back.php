f0b8ea581d7a29c9b82af271262842d03b4a68cd
jun katafuchi <katafuchi@c-limber.co.jp>
2016-03-19 17:06:53 +0900

    gii ganerate

---
+++ basic/controllers/CountryController_back.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+use yii\data\Pagination;
+use app\models\Country;
+
+class CountryController extends Controller
+{
+	public function actionIndex()
+	{
+		$query = Country::find();
+
+		$pagination = new Pagination([
+			'defaultPageSize' => 5,
+			'totalCount' => $query->count(),
+		]);
+
+		$countries = $query->orderBy('name')
+			->offset($pagination->offset)
+			->limit($pagination->limit)
+			->all();
+
+		// $country = Country::findOne('US');
+		// $country->name = 'U.S.A';
+		// $country->save();
+
+
+		return $this->render('index', [
+			'countries' => $countries,
+			'pagination' => $pagination,
+		]);
+	}
+}
